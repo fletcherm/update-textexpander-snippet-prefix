d3b61cced5ccd60b7230a26bf6a33d58a6806840
Matt Fletcher <fletcher@atomicobject.com>
2016-04-04 17:52:21 -0400

    Finished initial cut (and hopefully final version)

--- update-snippet-prefix.coffee
+++ update-snippet-prefix.coffee
@@ -1,1 +1,9 @@
-console.log 'hi'
+TextExpander = Application('TextExpander')
+for group in TextExpander.groups()
+  for snippet in group.snippets()
+    regex = /^;/
+    macro = snippet.abbreviation()
+    if macro.match(regex)
+      newMacro = macro.replace(regex, 'x')
+      console.log "Replacing [#{macro}] with [#{newMacro}]"
+      snippet.abbreviation = newMacro
--- update-snippet-prefix.js
+++ update-snippet-prefix.js
@@ -1,4 +1,22 @@
 (function() {
-  console.log('hi');
+  var TextExpander, group, i, j, len, len1, macro, newMacro, ref, ref1, regex, snippet;
+
+  TextExpander = Application('TextExpander');
+
+  ref = TextExpander.groups();
+  for (i = 0, len = ref.length; i < len; i++) {
+    group = ref[i];
+    ref1 = group.snippets();
+    for (j = 0, len1 = ref1.length; j < len1; j++) {
+      snippet = ref1[j];
+      regex = /^;/;
+      macro = snippet.abbreviation();
+      if (macro.match(regex)) {
+        newMacro = macro.replace(regex, 'x');
+        console.log("Replacing [" + macro + "] with [" + newMacro + "]");
+        snippet.abbreviation = newMacro;
+      }
+    }
+  }
 
 }).call(this);
